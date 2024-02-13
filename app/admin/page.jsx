02c16d4c7592ddec94c2dfc3c77d38948252a3ac
kezdetphia <fehermark88@gmail.com>
2024-02-13 12:51:18 -0800

    prep for deploy to test

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,14 +1,14 @@
 import Addpost from "@/components/admin/addPost";
-import AdminAddPostForm from "@/components/admin/adminAddPostForm";
-import AdminPostHandling from "@/components/admin/adminPostHandling";
-import AdminUserList from "@/components/admin/AdminUserList";
+// import AdminAddPostForm from "@/components/admin/adminAddPostForm";
+// import AdminPostHandling from "@/components/admin/adminPostHandling";
+// import AdminUserList from "@/components/admin/AdminUserList";
 import { auth } from "@/lib/auth";
 import { getPosts } from "@/lib/data/postData";
 import { getUsers } from "@/lib/data/userData";
 export default async function AdminPage() {
   const session = await auth();
   const userId = session.user.id;
-  const posts = await getPosts();
+  // const posts = await getPosts();
 
   const users = await getUsers();
   console.log(users);
