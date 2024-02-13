e711d72cd28644afdce3b6f1f06b024cbf3a4c9a
kezdetphia <fehermark88@gmail.com>
2024-02-13 12:48:32 -0800

    style

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -4,8 +4,7 @@ import { getPosts } from "@/lib/data/postData";
 export default async function Blog() {
   const posts = await getPosts();
 
-  //TODO: have 4 posts in a row on largse screens and oiut 1 on smaller screens
-  //Have all the images the same size
+
 
   return (
     <div className="px-10 py-4 flex flex-col md:flex-row items-center flex-wrap gap-4 justify-center pt-10">
