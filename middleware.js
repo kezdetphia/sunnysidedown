143d4c4545be2707a070fd1aeb75f074f15f8670
kezdetphia <fehermark88@gmail.com>
2024-02-17 23:20:54 -0800

    try to optimise for deployment

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./lib/auth.config";
-import { auth } from "./lib/auth";
 
 // export default auth((req) => {
 //   console.log("middelware");
@@ -13,11 +12,12 @@ export default NextAuth(authConfig).auth;
 export const config = {
   runtime: "experimental-edge",
   unstable_allowDynamic: [
-    "/lib/utilities.js", // allows a single file
+    "/lib/connecToDb.js", // allows a single file
+    "/lib/auth.js", // allows a single file
     "/node_modules/function-bind/**", // use a glob to allow anything in the function-bind 3rd party module
   ],
   // matcher: ["/((?!api|static|.*\\..*|_next).*)"],
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!api|static|.*\\..*|_next).*)"],
 
   // matcher: ["/((?!api|static|.*\\..*|_next)[^?#]*)"],
 };
