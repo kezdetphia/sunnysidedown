686350d7f2c2c654fda5ecaffbf00dfc4fd1751b
kezdetphia <fehermark88@gmail.com>
2024-03-17 12:48:52 -0700

    prep for deployment

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -8,8 +8,8 @@ export default async function Blog() {
     <div className=" px-10  py-20 flex flex-col md:flex-row items-center flex-wrap gap-4 justify-center bg-neutral-950  ">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-14">
         {posts.map((post) => (
-          <ul>
-            <li key={post.id}>
+          <ul key={post.id}>
+            <li>
               <Postcard post={post} />
             </li>
           </ul>
